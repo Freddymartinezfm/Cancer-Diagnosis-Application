fecc90ab5832c455ff92c6df045b79b0029bad6c
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-11-28 12:25:12 -0600

    added initial code from previous location

---
+++ Main.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+import view.Panel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setLocation(400, 100);
+        window.setTitle("Group 5 Breast Cancer Diagnosis Application");
+
+        var panel  = new Panel(window);
+        panel.init();
+
+        window.pack();
+        window.setVisible(true);
+
+    }
+
+
+}
+
---
+++ view/Canvas.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.awt.Dimension;
+
+import java.awt.Color;
+import java.awt.Font;
+
+
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Canvas extends JPanel {
+
+    private Panel panel;
+
+    public Canvas(Panel panel) {
+        this.panel = panel;
+        setPreferredSize(new Dimension(500, 550));
+        setBackground(Color.black);
+    }
+
+
+    // public void init(){
+    //     JTextField stuff = new JTextField("Here is where all the text will go in terms of success in connecting to the server and logs");
+    //     stuff.setForeground(Color.green);
+    //     stuff.setFont(new Font("Courier New", Font.BOLD, 12));
+
+
+    // }
+    
+    
+    
+}
+

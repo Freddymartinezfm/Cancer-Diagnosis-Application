122a724f4ca37772685ff8b05243779935b956e5
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-11-30 10:45:07 -0600

    Revert "Merge pull request #6 from Freddymartinezfm/ServerBranch"
    This reverts commit 09b5ebe270bcec578b28320669bdeb4119f14cee, reversing
    changes made to 2c311f5115bc9a410921b6baf7e4a4187d1f5394.

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
-import server_management.ServerStarter;
-import view.GUIStarter;
+import javax.swing.*;
+import java.awt.*;
 
+import view.Panel;
 public class Main {
 
     public static void main(String[] args) {
 
-        new GUIStarter();
-        new ServerStarter();
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setLocation(400, 100);
+        window.setTitle("Group 5 Breast Cancer Diagnosis Application");
+
+        var panel  = new Panel(window);
+        panel.init();
+
+        window.pack();
+        window.setVisible(true);
 
     }
 

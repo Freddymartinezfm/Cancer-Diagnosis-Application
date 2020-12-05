30737e1f9b024d9b1fffe5961b78f09bbea24a8e
freddymartinezfm1 <38080761+freddymartinezfm1@users.noreply.github.com>
2020-12-04 18:49:00 -0600

    changes to log4j.xml file and moved loggingManagerTest to correct directory

---
+++ team5/src/test/java/server/diagnosis/LoggingManagerTest.java
@@ -0,0 +1,18 @@
+package server.diagnosis;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+
+
+class LoggingManagerTest {
+
+	@Test
+    void setUp() {
+        // fail();
+    }
+
+    @AfterEach
+	void tearDown() {
+		// fail();
+	}
+}

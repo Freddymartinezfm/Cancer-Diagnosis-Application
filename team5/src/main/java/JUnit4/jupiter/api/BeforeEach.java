39f9ee45cef36010b96a277cbfc1f67564c81555
Freddy Martinez <freddymartinezfm@icloud.com>
2020-12-04 23:55:45 -0600

    beginning unit tests for several classes

--- team5/src/main/java/JUnit4/jupiter/api/BeforeEach.java
+++ team5/src/main/java/JUnit4/jupiter/api/BeforeEach.java
@@ -1,8 +1,18 @@
 package JUnit4.jupiter.api;
 
+import javax.swing.JTextArea;
+
+import server.diagnosis.view.GUIStarter;
+import server.diagnosis.view.Panel;
+
 public @interface BeforeEach {
 
-	String xml =  "<Dataset>\n" +
+
+	
+
+	
+
+	String positiveXml =  "<Dataset>\n" +
 	"    <id>1000025</id>\n" +
 	"    <thickness>5</thickness>\n" +
 	"    <uniformity_size>1</uniformity_size>\n" +
@@ -15,6 +25,23 @@ public @interface BeforeEach {
 	"    <mitoses>1</mitoses>\n" +
 	"    <class>2</class>\t//this is what our intelligent model determines\n" +
 	"</Dataset>";
-	
+
+	String negativeXml =  "<Dataset>\n" +
+	"    <id>1000025</id>\n" +
+	"    <thickness>5</thickness>\n" +
+	"    <uniformity_size>1</uniformity_size>\n" +
+	"    <uniformity_shape>1</uniformity_shape>\n" +
+	"    <adhesion>1</adhesion>\n" +
+	"    <epithelial_size>2</epithelial_size>\n" +
+	"    <bare_nuclei>1</bare_nuclei>\n" +
+	"    <chromatin>3</chromatin>\n" +
+	"    <nucleoli>1</nucleoli>\n" +
+	"    <mitoses>1</mitoses>\n" +
+	"    <class>2</class>\t//this is what our intelligent model determines\n" +
+	"</Dataset>";
+
+
+
+
 
 }

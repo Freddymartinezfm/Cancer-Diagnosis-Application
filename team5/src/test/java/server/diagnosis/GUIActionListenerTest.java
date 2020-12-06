9622dbbcc96fd60c5d7987363c3d4a1d032f898e
Freddy Martinez <freddymartinezfm@icloud.com>
2020-12-06 01:29:31 -0600

    skeleton test classes a

---
+++ team5/src/test/java/server/diagnosis/GUIActionListenerTest.java
@@ -0,0 +1,23 @@
+package server.diagnosis;
+
+import javax.swing.JFrame;
+
+import org.junit.Test;
+
+import JUnit4.jupiter.api.BeforeEach;
+import server.diagnosis.controller.GUIActionListener;
+import server.diagnosis.view.GUIStarter;
+import server.diagnosis.view.Panel;
+
+public class GUIActionListenerTest {
+
+
+	@Test
+	public void testSaveForm() {
+		// GUIActionListener guiInstance = new GUIActionListener(this);
+	}
+
+
+	// test validateForm()
+	
+}

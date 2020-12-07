efcfbe50b0604321252a99417c49d394fecaa412
Susan Emberton <darkprincess@gmail.com>
2020-12-07 12:42:09 -0600

    added saveForm() test and moved picture

--- team5/src/test/java/server/diagnosis/GUIActionListenerTest.java
+++ team5/src/test/java/server/diagnosis/GUIActionListenerTest.java
@@ -1,6 +1,7 @@
 package server.diagnosis;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 import org.junit.Test;
 
@@ -10,11 +11,17 @@ import server.diagnosis.view.GUIStarter;
 import server.diagnosis.view.Panel;
 
 public class GUIActionListenerTest {
-
+	
+	JFrame window1 = new JFrame();
+	Panel panel = new Panel(window1);
+	GUIActionListener guiInstance = new GUIActionListener(panel);
+	String[] patientTest = guiInstance.getPatientInfo();;
 
 	@Test
 	public void testSaveForm() {
-		// GUIActionListener guiInstance = new GUIActionListener(this);
+		
+		guiInstance.fillTextFieldOne();
+		guiInstance.saveForm();
 	}
 
 

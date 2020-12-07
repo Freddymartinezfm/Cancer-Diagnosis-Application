106499293c429def28584d2b1cb9a2df67157ba5
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-12-07 00:53:49 -0600

    client connect/diagnose working

--- team5/src/main/java/server/diagnosis/server_management/Client.java
+++ team5/src/main/java/server/diagnosis/server_management/Client.java
@@ -16,7 +16,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Client {
-    private static final Logger logger = LogManager.getLogger();
+	private static final Logger logger = LogManager.getLogger();
+	
 	
 	public Client(){
 		logger.info("Client stub log");
@@ -31,15 +32,15 @@ public class Client {
 		InputStreamReader isr;
 		// add more port numbers to peerPorts to test multiple systems
 		int[] peerPorts = {7123};
+		// diagnoses array size needs to match number of peerPorts
+		int[] diagnoses = new int[1];
+		String s;
+		int diagnosesCount = 0;
 
 		for (int p: peerPorts) {
-			try(Socket socket = new Socket("localhost", p)){
-				BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
-				// now get info from user or oher server
-				Scanner sc = new Scanner(System.in);
-				String echoString; // object to send back to server
 
+			try(Socket socket = new Socket("localhost", p)){
+				
 				//start of convert file to byte data
 				File xmlFile = new File("patient_info.xml");
 				InputStream inFile = new FileInputStream(xmlFile);
@@ -50,23 +51,21 @@ public class Client {
 					outFile.write(bytes, 0, count);
 				}
 				inFile.close();
+				outFile.close();
 				//end
 
-				String response;
-				/*do {
-					System.out.println("enter data to be echoed");
-					echoString = sc.nextLine();
-					stringToEcho.println(echoString); //originally echoString
-					if (!echoString.equals("exit")) {
-						response = echoes.readLine();
-						System.out.println(response + " other data to be be sent back");
-					}
-				} while (!echoString.equals("exit"));*/
+				BufferedReader echoes = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				s = echoes.readLine();
+				diagnoses[count] = Integer.parseInt(s);
+				echoes.close();
+				
 			} catch(IOException e){
 				System.out.println("uh oh" + e.getMessage());
 			}
+			diagnosesCount++;
 		}
-		
+		for(int e: diagnoses) {
+			System.out.println(e);
+		}
 	}
-
 }
--- team5/src/main/java/server/diagnosis/server_management/Peer.java
+++ team5/src/main/java/server/diagnosis/server_management/Peer.java
@@ -1,6 +1,8 @@
 package server.diagnosis.server_management;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,91 +12,67 @@ import org.apache.logging.log4j.Logger;
 import server.diagnosis.Diagnosis;
 import server.diagnosis.DiagnosisImplementation;
 import server.diagnosis.StubImplementation;
+import server.diagnosis.component.xmlparser.xmlManager;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class Peer extends Thread {
-	private static final Logger logger = LogManager.getLogger(Peer.class);
 
 	private Socket socket;
+	private static final Logger logger = LogManager.getLogger(Peer.class);
+
 
 	public Peer (Socket socket){
-		logger.info("Client socket port:  " +  socket.getLocalPort());
-		logger.info("Client address: " + socket.getInetAddress());
+		System.out.println("Client connected:"  + socket.getChannel());
+		System.out.println("Client socket port:  " +  socket.getLocalPort());
+		System.out.println("Client address: " + socket.getInetAddress());
 		this.socket = socket;
 	}
 
 
 	@Override
 	public void run(){
-		try {
-			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-			String echoString = " ";
-
-			while (true){
-				echoString = input.readLine();
-				logger.info(echoString);
-				logger.info("response contains:  " + echoString +  "diagnosis result");
-				output.println("response contains:  " + echoString +  "diagnosis result");
-				
-
-
-				// diagnosis
-				// DiagnosisImplementation impl = new StubImplementation();
-				// Diagnosis diag = new Diagnosis(impl);
-				// System.out.println(diag);
-				// socket.setSoTimeout(5000);
-				if (echoString.equals("exit")){
-					logger.info("Client requested to close server: ");
-					break;
-				}
-
-
-				/*
-
-				//create new xmlfile at location to parse
-					File outputFile = new File("patient_info2.xml");
-					InputStream in = socket.getInputStream();
-					OutputStream out = new FileOutputStream(outputFile);
-					byte[] bytes = new byte[2*1024];
-					int count;
-					while ((count = in.read(bytes)) > 0) {
-						out.write(bytes, 0, count);
-					}
-
-					xmlManager manager = new xmlManager();
-					manager.parse(outputFile);
-
-					DiagnosisImplementation impl = new StubImplementation();
-					Diagnosis diag = new Diagnosis(impl);
-					//diag.runDiagnosis(manager.getInfo());
-					System.out.println(diag.runDiagnosis(manager.getInfo()));
-					s = input.readLine();
-					logger.info("remote connection: " +  s);
-					output.println("response " + " diag result ");
-					System.out.println("read " + s + " from connection ");
-				 */
-
-				new Peer(socket);
+		try {						
+			//create new xmlfile at location to parse
+			File outputFile = new File("patient_info2.xml");
+			InputStream in = socket.getInputStream();
+			OutputStream out = new FileOutputStream(outputFile);
+			byte[] bytes = new byte[2*1024];
+			int count;
+			while ((count = in.read(bytes)) > 0) {
+				out.write(bytes, 0, count);
 			}
+			out.close();
+			in.close();
 
+			xmlManager manager = new xmlManager();
+			manager.parse(outputFile);
+
+			DiagnosisImplementation impl = new StubImplementation();
+			int diagnosis = impl.runDiagnosis(manager.getInfo());
+
+			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+			String message = Integer.toString(diagnosis);
+			output.write(message);
+			output.close();
+			
 		} catch (IOException e){
-			logger.info("Oops: " +e.getMessage());
+			System.out.println("Oops no client connnection: " +e.getMessage());
+
 		} finally {
 			try {
 				socket.close();
 			} catch (IOException e){
 				// a connection has been closed
-				logger.info("Oops: " +e.getMessage());
+				System.out.println("connection closed: "  + e.getMessage());
 
 			}
 		}

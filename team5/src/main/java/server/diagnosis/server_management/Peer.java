7dcadbb367b6eb3b786d7898b47bebaf337c42cd
chase minden <caminden@gmail.com>
2020-12-07 03:34:56 -0600

    Finish socket connections
    also reworked diagnose to more random

--- team5/src/main/java/server/diagnosis/server_management/Peer.java
+++ team5/src/main/java/server/diagnosis/server_management/Peer.java
@@ -15,6 +15,7 @@ import server.diagnosis.StubImplementation;
 import server.diagnosis.component.xmlparser.xmlManager;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -29,29 +30,30 @@ public class Peer extends Thread {
 	private Socket socket;
 	private static final Logger logger = LogManager.getLogger(Peer.class);
 
-
-	public Peer (Socket socket){
-		System.out.println("Client connected:"  + socket.getChannel());
-		System.out.println("Client socket port:  " +  socket.getLocalPort());
+	public Peer(Socket socket) {
+		System.out.println("Client connected:" + socket.getChannel());
+		System.out.println("Client socket port:  " + socket.getLocalPort());
 		System.out.println("Client address: " + socket.getInetAddress());
 		this.socket = socket;
 	}
 
-
 	@Override
-	public void run(){
-		try {						
-			//create new xmlfile at location to parse
+	public void run() {
+		try {
+			// create new xmlfile at location to parse
+			System.out.println("File transfer start");
+			
 			File outputFile = new File("patient_info2.xml");
 			InputStream in = socket.getInputStream();
 			OutputStream out = new FileOutputStream(outputFile);
-			byte[] bytes = new byte[2*1024];
+			byte[] bytes = new byte[2 * 1024];
 			int count;
 			while ((count = in.read(bytes)) > 0) {
 				out.write(bytes, 0, count);
 			}
 			out.close();
-			in.close();
+			//in.close; //closing this closes the socket
+			System.out.println("File recieved");
 
 			xmlManager manager = new xmlManager();
 			manager.parse(outputFile);
@@ -59,20 +61,22 @@ public class Peer extends Thread {
 			DiagnosisImplementation impl = new StubImplementation();
 			int diagnosis = impl.runDiagnosis(manager.getInfo());
 
-			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-			String message = Integer.toString(diagnosis);
-			output.write(message);
-			output.close();
-			
-		} catch (IOException e){
-			System.out.println("Oops no client connnection: " +e.getMessage());
+			//new socket to return results to client
+			Socket socket1 = new Socket("localhost", 5000);
+			OutputStream output = new DataOutputStream(socket1.getOutputStream());
+			output.write(diagnosis);
+			socket1.close();
+			//end
+
+		} catch (IOException e) {
+			System.out.println("Oops no client connnection: " + e.getMessage());
 
 		} finally {
 			try {
 				socket.close();
-			} catch (IOException e){
+			} catch (IOException e) {
 				// a connection has been closed
-				System.out.println("connection closed: "  + e.getMessage());
+				System.out.println("connection closed: " + e.getMessage());
 
 			}
 		}

1ae13439509daa023ba10117bedec3b324ad5cb7
Freddy Martinez <freddymartinezfm@icloud.com>
2020-12-07 00:05:44 -0600

    working state

--- team5/src/main/java/server/diagnosis/server_management/Peer.java
+++ team5/src/main/java/server/diagnosis/server_management/Peer.java
@@ -1,8 +1,6 @@
 package server.diagnosis.server_management;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,26 +10,18 @@ import org.apache.logging.log4j.Logger;
 import server.diagnosis.Diagnosis;
 import server.diagnosis.DiagnosisImplementation;
 import server.diagnosis.StubImplementation;
-import server.diagnosis.component.xmlparser.xmlManager;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Peer extends Thread {
 
 	private Socket socket;
-	private static final Logger logger = LogManager.getLogger(Peer.class);
-
 
 	public Peer (Socket socket){
-		System.out.println("Client connected:"  + socket.getChannel());
 		System.out.println("Client socket port:  " +  socket.getLocalPort());
 		System.out.println("Client address: " + socket.getInetAddress());
 		this.socket = socket;
@@ -43,20 +33,28 @@ public class Peer extends Thread {
 		try {
 			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-			InetAddress ip = socket.getInetAddress();
-			
-			String s = " ";
-			while (true) { 
-				if (ip.isLoopbackAddress()){  // local connection
-					// System.out.println("Local connection ");
-					output.println("Local connection ");
-					logger.info("local");
-					s = input.readLine();
-					System.out.println(s);
-					output.println("response contains:  " + s +  "diagnosis result");
-				} else { // remote connection
-					System.out.println("Remote connection ");
-					//create new xmlfile at location to parse
+			String echoString = " ";
+
+			while (true){
+				echoString = input.readLine();
+				System.out.println(echoString);
+				output.println("response contains:  " + echoString +  "diagnosis result");
+
+
+				// diagnosis
+				// DiagnosisImplementation impl = new StubImplementation();
+				// Diagnosis diag = new Diagnosis(impl);
+				// System.out.println(diag);
+				// socket.setSoTimeout(5000);
+				if (echoString.equals("exit")){
+					System.out.println("Client requested to close server: ");
+					break;
+				}
+
+
+				/*
+
+				//create new xmlfile at location to parse
 					File outputFile = new File("patient_info2.xml");
 					InputStream in = socket.getInputStream();
 					OutputStream out = new FileOutputStream(outputFile);
@@ -77,28 +75,13 @@ public class Peer extends Thread {
 					logger.info("remote connection: " +  s);
 					output.println("response " + " diag result ");
 					System.out.println("read " + s + " from connection ");
-					
-				}
+				 */
+
 				new Peer(socket);
-
-
-				// Diagnosis implementation goes below here, outside of if else statement 
-				// either way a diagnosis will have to be completed, and can reside outside the loop
-				// loop indefinately reading the input from the connection.
-				// if (s.equals("exit")){
-				// 	System.out.println("Client requested to close server: ");
-				// 	break;
-				// }
-
-			}			
-
-			// moved to scratch.txt
-
-			// if code reaches here, a client did not connect 
+			}
 
 		} catch (IOException e){
-			System.out.println("Oops no client connnection: " +e.getMessage());
-
+			System.out.println("Oops: " +e.getMessage());
 		} finally {
 			try {
 				socket.close();

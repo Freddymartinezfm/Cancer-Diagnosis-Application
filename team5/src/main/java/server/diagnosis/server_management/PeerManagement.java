d9f41b2b82458b73769174837f5cb26a0dfd8bc6
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-12-07 00:34:16 -0600

    client connect/diagnose working

--- team5/src/main/java/server/diagnosis/server_management/PeerManagement.java
+++ team5/src/main/java/server/diagnosis/server_management/PeerManagement.java
@@ -22,7 +22,6 @@ public class PeerManagement {
 	}
 
 	public void start(){
-
 		
 		try (ServerSocket serverSocket = new ServerSocket(7123)){ // try with resources
 			logger.info("System is ready to serve requests.");
